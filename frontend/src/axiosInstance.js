988220fbbdc20c6e9f994a3f1ccf4f1869743a9c
Aditya <suniladityajatni@gmail.com>
2022-10-20 17:43:34 +0530

    Fixed a bug

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:9000/"
-    //https://remind-meapp.herokuapp.com/
+    baseURL:"https://remind-meapp.herokuapp.com/"
 })
 export default axiosInstance;
